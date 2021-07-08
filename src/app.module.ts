6cdf2d7d9ee459fb93631dd0f53c171f8a9dd7f8
RoxKilly <4ibi6jrdj3ynzgxy@protonmail.com>
2021-07-08 03:49:01 -0400

    Remove unneeded code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,57 +1,12 @@
-import {
-  CACHE_MANAGER,
-  CacheModule,
-  Inject,
-  Logger,
-  Module,
-  OnApplicationBootstrap,
-} from '@nestjs/common';
+import { Module, OnApplicationBootstrap } from '@nestjs/common';
 import { AppController } from './app.controller';
-import * as redisStore from 'cache-manager-redis-store';
-import { Cache } from 'cache-manager';
-import { timer } from 'rxjs';
 
 @Module({
-  imports: [
-    CacheModule.register({
-      store: redisStore,
-      host: 'localhost',
-      port: 6379,
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
 })
 export class AppModule implements OnApplicationBootstrap {
-  constructor(@Inject(CACHE_MANAGER) private cache: Cache) {}
-
   async onApplicationBootstrap() {
-    // empty the cache
-    Logger.log('Reset the cache');
-    await this.cache.reset();
-    const TTL_IS_NULL_KEY = 'TTL_IS_NULL_KEY';
-    const TTL_IS_ZERO_KEY = 'TTL_IS_ZERO_KEY';
-    const TTL_IS_NULL_VALUE = 'TTL_IS_NULL_VALUE';
-    const TTL_IS_ZERO_VALUE = 'TTL_IS_ZERO_VALUE';
-
-    await timer(5000).toPromise();
-
-    Logger.log('Set cache items');
-    await this.cache.set(TTL_IS_NULL_KEY, TTL_IS_NULL_VALUE, { ttl: null });
-    await this.cache.set(TTL_IS_ZERO_KEY, TTL_IS_ZERO_VALUE, { ttl: 0 });
-    await timer(5000).toPromise();
-
-    Logger.log('Read cache');
-    this.logResult(TTL_IS_NULL_KEY, await this.cache.get(TTL_IS_NULL_KEY));
-    this.logResult(TTL_IS_ZERO_KEY, await this.cache.get(TTL_IS_ZERO_KEY));
-  }
-
-  private logResult(key, value) {
-    if (value === undefined || value === null) {
-      Logger.error(
-        `Key ${key} was not in the cache. Value returned was ${value}`,
-      );
-    } else {
-      Logger.log(`Key ${key} was read from the cache. Value is ${value}`);
-    }
+    return;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { RedisOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  await NestFactory.createMicroservice<RedisOptions>(AppModule, {
-    transport: Transport.REDIS,
-    options: { url: 'redis://localhost:6379' },
-  });
   await app.listen(3000);
 }
+
 bootstrap();
